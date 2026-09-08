56b9326b7e26cd2873289eb934198d186a2ebc76
WebCrew <palareas@gmail.com>
2026-09-08 09:12:01 +0200

    feat: expose project scene opening

--- src/main/preload.js
+++ src/main/preload.js
@@ -6,6 +6,7 @@ contextBridge.exposeInMainWorld('parlynHost', {
   openScene:()=>ipcRenderer.invoke('parlyn:scene:open'),
   createProject:(payload)=>ipcRenderer.invoke('parlyn:project:create',payload),
   openProject:()=>ipcRenderer.invoke('parlyn:project:open'),
+  openProjectScene:(payload)=>ipcRenderer.invoke('parlyn:project:open-scene',payload),
   closeProject:()=>ipcRenderer.invoke('parlyn:project:close'),
   deleteProject:(payload)=>ipcRenderer.invoke('parlyn:project:delete',payload),
   saveProjectScene:(payload)=>ipcRenderer.invoke('parlyn:project:save-scene',payload),
